0135d42e59d22b1b41644af907a4e035014a7fa9
chicken-fingers-backup <aliyahanderson98@gmail.com>
2025-09-25 18:20:43 -0500

    Defined a function for the welcome message

--- javascript/external.js
+++ javascript/external.js
@@ -1,16 +1,26 @@
-document.writeIn("<button onclick='condition()' >Conditional Test</button>");
-function condition()
+// document.writeIn("<button onclick='condition()' >Conditional Test</button>");
+// function condition()
+// {
+//   x = confirm("Are you sure you want to proceed?");
+//   if(x)
+//   {
+//     document.writeIn("You chose Okay!");
+//   }
+//   else
+//   {
+//     document.writeIn("You chose Cancel!");
+//   }
+// }
+// //  a prompt box is ussed to prompt users to input a value before entering a page.
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+function welcome()
 {
-  x = confirm("Are you sure you want to proceed?");
-  if(x)
-  {
-    document.writeIn("You chose Okay!");
-  }
-  else
-  {
-    document.writeIn("You chose Cancel!");
-  }
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+// A prompt box is used to prompt users to input a value before entering a page.
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+  return message
 }
-//  a prompt box is ussed to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+document.write(welcome());

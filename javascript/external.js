da3c0bfd8e7fe711610515a6cc5de2eac87ac739
chicken-fingers-backup <aliyahanderson98@gmail.com>
2025-10-02 01:21:53 -0500

    Filled content from webmaps array into the webmaps table

---
+++ javascript/external.js
@@ -0,0 +1,47 @@
+// document.writeIn("<button onclick='condition()' >Conditional Test</button>");
+// function condition()
+// {
+//   x = confirm("Are you sure you want to proceed?");
+//   if(x)
+//   {
+//     document.writeIn("You chose Okay!");
+//   }
+//   else
+//   {
+//     document.writeIn("You chose Cancel!");
+//   }
+// }
+// //  a prompt box is ussed to prompt users to input a value before entering a page.
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+// A prompt box is used to prompt users to input a value before entering a page.
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+  return message
+}
+document.write(welcome());
+
+function webmap_table()
+{
+  document.write("<table width=100%>");
+  for (var row=0; row < webmaps/length; row++)
+  {
+    document.write('<tr>');
+    for (var column=0; column < webmaps[0].length; column++)
+    {
+      document.write("<td>" + webmaps[row][column] + "</td>");
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+  return "";
+}
+var webmaps =
+[
+  ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit developed by Enterprise Technology Solutions is neat."],
+  ["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The Texas Parks and Wildlife's Landscape Ecology program is great."]
+];

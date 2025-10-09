3681632607558050f16d858a9e50f5c52f52daf6
chicken-fingers-backup <aliyahanderson98@gmail.com>
2025-10-09 06:04:40 -0500

    fixed code

--- javascript/external.js
+++ javascript/external.js
@@ -30,7 +30,7 @@ function webmap_table()
   document.write("<table width=100%>");
   for (var row=0; row < webmaps.length; row++)
   {
-    document.write('<tr>');
+    document.write("<tr>");
     for (var column=0; column < webmaps[0].length; column++)
     {
       document.write("<td>" + webmaps[row][column] + "</td>");
